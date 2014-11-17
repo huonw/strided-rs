905e7e8992dd724887190c73ec4f670dcaaaca3f
Huon Wilson <dbau.pp+github@gmail.com>
2014-11-17 18:24:38 +1100

    Add the FFT example.

--- src/lib.rs
+++ src/lib.rs
@@ -117,6 +117,93 @@
 //! The shared `Strided` is equivalent to `&[]` and only handles `&`
 //! references, making ownership transfer and `reborrow` unnecessary,
 //! so all its methods act identically to those on `&[]`.
+//!
+//! # Example
+//!
+//! The [fast Fourier transform
+//! (FFT)](https://en.wikipedia.org/wiki/Fast_Fourier_transform) is a
+//! signal processing algorithm that performs a discrete Fourier
+//! transform (DFT) of length `n` in `O(n log n)` time. A DFT breaks a
+//! waveform into the sum of sines and cosines, and is an important
+//! part of many other algorithms due to certain nice properties of
+//! the Fourier transform.
+//!
+//! The first FFT algorithm was the [Cooley-Tukey
+//! algorithm](https://en.wikipedia.org/wiki/Cooley-Tukey_FFT_algorithm). The
+//! decimation-in-time variant works by computing the FFT of
+//! equal-length subarrays of equally spaced elements and then
+//! combining these together into the desired result. This sort of
+//! spacing is exactly the striding provided by this library, and
+//! hence this library can be used to create an FFT algorithm in a
+//! very natural way.
+//!
+//! Below is an implementation of the radix-2 case, that is, when the
+//! length `n` is a power of two, in this case, only two strided
+//! subarrays are necessary: exactly the alternating ones provided by
+//! `substrides2`. Note the use of `reborrow` to allow `start` and
+//! `end` to be used for the recursive `fft` calls and then again
+//! later in the loop.
+//!
+//! ```rust
+//! extern crate strided;
+//! extern crate num; // https://github.com/rust-lang/num
+//! use std::num::{Int, Float};
+//! use num::complex::{Complex, Complex64};
+//! use strided::{MutStrided, Strided};
+//!
+//! /// Writes the forward DFT of `input` to `output`.
+//! fn fft(input: Strided<Complex64>, mut output: MutStrided<Complex64>) {
+//!     // check it's a power of two.
+//!     assert!(input.len() == output.len() && input.len().count_ones() == 1);
+//!
+//!     // base case: the DFT of a single element is itself.
+//!     if input.len() == 1 {
+//!         output[0] = input[0];
+//!         return
+//!     }
+//!
+//!     // split the input into two arrays of alternating elements ("decimate in time")
+//!     let (evens, odds) = input.substrides2();
+//!     // break the output into two halves (front and back, not alternating)
+//!     let (mut start, mut end) = output.split_at(input.len() / 2);
+//!
+//!     // recursively perform two FFTs on alternating elements of the input, writing the
+//!     // results into the first and second half of the output array respectively.
+//!     fft(evens, start.reborrow());
+//!     fft(odds, end.reborrow());
+//!
+//!     let two_pi: f64 = Float::two_pi();
+//!     // exp(-2πi/N)
+//!     let twiddle = Complex::from_polar(&1.0, &(-two_pi / input.len() as f64));
+//!
+//!     let mut factor = Complex::one();
+//!
+//!     // combine the subFFTs with the relations:
+//!     //   X_k = E_k + exp(-2πki/N) * O_k
+//!     //   X_{k+N/2} = E_k - exp(-2πki/N) * O_k
+//!     for (even, odd) in start.iter_mut().zip(end.iter_mut()) {
+//!         let twiddled = factor * *odd;
+//!         let e = *even;
+//!
+//!         *even = e + twiddled;
+//!         *odd = e - twiddled;
+//!         factor = factor * twiddle;
+//!     }
+//! }
+//!
+//! fn main() {
+//!     let a = [Complex::new(2., 0.), Complex::new(1., 0.),
+//!              Complex::new(2., 0.), Complex::new(1., 0.)];
+//!     let mut b = [Complex::new(0., 0.), .. 4];
+//!
+//!     fft(Strided::new(&a), MutStrided::new(&mut b));
+//!     println!("forward: {} -> {}", a.as_slice(), b.as_slice());
+//! }
+//! ```
+//!
+//! The above definitely has complexity `O(n log n)`, but it has a
+//! much larger constant factor than an optimised library like
+//! [FFTW](http://www.fftw.org/).
 
 #[cfg(test)] extern crate test;
 
